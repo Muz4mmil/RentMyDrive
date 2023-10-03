a7ac4253865a9765d07bef213bb249a3f04928df
Muzammil <muzammilsiddiqui826@gmail.com>
2023-10-03 18:03:40 +0530

    Project deployment

--- src/components/CarCard.jsx
+++ src/components/CarCard.jsx
@@ -5,10 +5,10 @@ const CarCard = ({ car }) => {
 
   return (
     <div className="car-card w-80 p-4 bg-slate-100 shadow-xl rounded-xl">
-      <img src={image} alt={name} className=' w-full h-44 rounded-lg bg-slate-300 object-cover'/>
+      <img src={image} alt={name} className='w-full h-44 rounded-lg bg-slate-300 object-cover'/>
 
       <div className="car-details">
-        <h3 className=' text-xl my-3'>{name}</h3>
+        <h3 className=' text-xl my-3'>{name} </h3>
 
         <div className="feature grid grid-cols-2 text-sm border-b-2 pb-3">
           <p className='flex items-center gap-2'><span className="material-symbols-rounded text-lg text-sky-500">group</span> {seatingCapacity}</p>
